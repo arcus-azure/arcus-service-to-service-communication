d96c07b227d4ec5f40cac16cc4ccf3bb9b34e0e6
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2023-01-06 13:56:55 +0100

    pr-fix: remove dedicated arcus http dependency tracking

--- src/Arcus.Shared/Services/Interfaces/IBaconService.cs
+++ src/Arcus.Shared/Services/Interfaces/IBaconService.cs
@@ -7,6 +7,5 @@ namespace Arcus.Shared.Services.Interfaces
     public interface IBaconService
     {
         Task<List<string>> GetBaconAsync();
-        Task<List<string>> GetBaconAsync(MessageCorrelationInfo messageCorrelationInfo);
     }
 }
